d37f8a1f92f06f8f564e2e955fd8351445fa855c
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-11-03 23:23:46 +0900

    Create BJP4 Self-Check 1.21: methodHeaderSyntax.java

---
+++ BJP-Chapter1-Introduction-to-Java-Programming/BJP4 Self-Check 1.21: methodHeaderSyntax.java
@@ -0,0 +1,12 @@
+/*
+Which of the following method headers uses the correct syntax?
+
+a. public static example void[] {
+b. public void static example() {
+c. public static example() {
+d. public void static example{} (
+e. public static void example() {
+(order shuffled)
+*/
+
+e. public static void example() {
