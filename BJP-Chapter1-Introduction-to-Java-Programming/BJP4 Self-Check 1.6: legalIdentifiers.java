9bdd001069de59a068fdc521b202fb0ce1445aa1
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-11-03 22:48:21 +0900

    Create BJP4 Self-Check 1.6: legalIdentifiers.java

---
+++ BJP-Chapter1-Introduction-to-Java-Programming/BJP4 Self-Check 1.6: legalIdentifiers.java
@@ -0,0 +1,21 @@
+/*
+Which of the following can be used in a Java program as identifiers? Check all of the identifiers that are legal.
+a. sum_of_data
+b. 42isThesolution
+c. for
+d. B4
+e. AnnualSalary
+f. ABC
+g. first-name
+h. "hello"
+i. println
+j. _average
+(order shuffled)
+*/
+
+a. sum_of_data
+d. B4
+e. AnnualSalary
+f. ABC
+i. println
+j. _average
