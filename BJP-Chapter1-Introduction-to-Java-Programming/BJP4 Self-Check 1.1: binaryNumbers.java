65ebdfaafa5915983b119da0c1bfc80bba88ca15
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-11-03 22:19:18 +0900

    Update and rename BJP4 Self-Check 1.1: binaryNumbers to BJP4 Self-Check 1.1: binaryNumbers.java

---
+++ BJP-Chapter1-Introduction-to-Java-Programming/BJP4 Self-Check 1.1: binaryNumbers.java
@@ -0,0 +1,11 @@
+/*
+Why do computers use binary numbers?
+ a. At the time when computers were invented, humans only knew about the numbers 0 and 1.
+ b. It makes the computer faster and more efficient than other number bases.
+ c. It is impossible to build a computer that uses a different number base than binary.
+ d. Because there are 10 types of people: Those who understand binary, and those who don't.
+ e. It is easier to build electronic devices reliably if they only have to distinguish between two electric states.
+(order shuffled)
+*/
+
+e. It is easier to build electronic devices reliably if they only have to distinguish between two electric states.
