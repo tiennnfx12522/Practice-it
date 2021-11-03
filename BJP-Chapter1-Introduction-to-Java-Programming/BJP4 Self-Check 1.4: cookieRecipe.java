7e6e42c55db43f337a6c624b0867993dfaacaccc
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-11-03 22:38:38 +0900

    Create BJP4 Self-Check 1.4: cookieRecipe.java

---
+++ BJP-Chapter1-Introduction-to-Java-Programming/BJP4 Self-Check 1.4: cookieRecipe.java
@@ -0,0 +1,12 @@
+/*
+In your own words, describe an algorithm for baking cookies. Assume that you have a large number of hungry friends, so you'll want to produce several batches of cookies!
+
+(Since there are so many correct answers, Practice-It will accept any solution of sufficient length.)
+*/
+
+cookieRecipe() {
+   prepWork();
+   makeBatter();
+   bake();
+   serve();
+}
