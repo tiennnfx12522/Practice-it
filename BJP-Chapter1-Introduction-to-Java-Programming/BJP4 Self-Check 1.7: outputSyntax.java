04d0e784a4f28d2298ac20bde8b0c36ad1cb5a29
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-11-03 22:49:46 +0900

    Create BJP4 Self-Check 1.7: outputSyntax.java

---
+++ BJP-Chapter1-Introduction-to-Java-Programming/BJP4 Self-Check 1.7: outputSyntax.java
@@ -0,0 +1,10 @@
+/*
+Which of the following is the correct syntax to output a message?
+a. System.println.out('Hello, world!');
+b. System.println(Hello, world!);
+c. System.println("Hello, world!");
+d. System.out.println("Hello, world!");
+e. Out.system.println"(Hello, world!)";
+*/
+
+d. System.out.println("Hello, world!");
