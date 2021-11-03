0523eaf2984b3b165b7ea06eaa6d0d99247875e6
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-11-03 22:59:31 +0900

    Create BJP4 Self-Check 1.11: downwardSpiral.java

---
+++ BJP-Chapter1-Introduction-to-Java-Programming/BJP4 Self-Check 1.11: downwardSpiral.java
@@ -0,0 +1,16 @@
+/*
+What is the output produced from the following statements? (Treat tabs as aligning to every multiple of eight spaces.)
+
+System.out.println("\\\\");
+System.out.println("'");
+System.out.println("\ta\tb\tc");
+System.out.println("\"\"\"");
+System.out.println("C:\nin\the downward spiral");
+*/
+
+\\
+'
+        a       b       c
+"""
+C:
+in      he downward spiral
