0e0c3ac61e6743a92cd476681433fbf688e1a814
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-11-05 23:48:02 +0900

    Create BJP4 Self-Check 2.15: firstSecond2.java

---
+++ BJP-Chapter2-Primitive-Data-and-Definite-Loops/BJP4 Self-Check 2.15: firstSecond2.java
@@ -0,0 +1,16 @@
+/*
+Rewrite the following code from the previous exercise to be shorter, by declaring the variables together on the same line, and by using the special assignment operators (e.g., +=, -=, *=, and /=) as appropriate.
+
+int first = 8;
+int second = 19;
+first = first + second;
+second = first - second;
+first = first - second;
+*/
+
+//shortend code
+
+int first = 8, second = 19;
+first += second;
+second = first - second;
+first -= second;
