8b5e0b6dca12813ae12b0ed97c7335ab9facbfc2
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-11-05 23:35:18 +0900

    Create BJP4 Self-Check 2.06: declareRealNumberSyntax.java

---
+++ BJP-Chapter2-Primitive-Data-and-Definite-Loops/BJP4 Self-Check 2.06: declareRealNumberSyntax.java
@@ -0,0 +1,11 @@
+/*
+Which of the following choices is the correct syntax for declaring a real number variable named 'grade' and initializing its value to 4.0?
+a. double grade = 4.0;
+b. grade = 4;
+c. 4.0 = grade;
+d. grade = double 4.0;
+e. int grade : 4.0;
+(order shuffled)
+*/
+
+a. double grade = 4.0;
