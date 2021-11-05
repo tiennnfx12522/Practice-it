73f9bb61abcdb290c20c994698cb41dead95cfe1
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-11-05 23:57:38 +0900

    Create BJP4 Self-Check 2.23: completeLoop.java

---
+++ BJP-Chapter2-Primitive-Data-and-Definite-Loops/BJP4 Self-Check 2.23: completeLoop.java
@@ -0,0 +1,16 @@
+/*
+Complete the following for loop to produce the following output:
+-4
+14
+32
+50
+68
+86
+for (int i = 1; i <= 6; i++) {
+    // your code here
+}
+*/
+
+for (int i = 1; i <= 6; i++) {
+    System.out.println(-22 + 18 * i);
+}
