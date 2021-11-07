eb2b012a08726a8a1141869314047ed01d53a497
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-11-08 00:01:29 +0900

    Create BJP4 Exercise 2.04: starSquare.java

---
+++ BJP-Chapter2-Primitive-Data-and-Definite-Loops/BJP4 Exercise 2.04: starSquare.java
@@ -0,0 +1,16 @@
+/*
+Write for loops to produce the following output:
+
+*****
+*****
+*****
+*****
+
+*/
+
+for (int i = 0; i < 4; i++) {
+    for (int j = 1; j <=5; j++) {
+    System.out.print("*");
+    }
+    System.out.println();                 
+}
