29df0bc9c2a42fa340d362649bbface799282597
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-11-08 21:45:54 +0900

    Create BJP4 Self-Check 3.1: methodHeaderSyntax.java

---
+++ BJP4 Chapter 3: Parameters and Objects/BJP4 Self-Check 3.1: methodHeaderSyntax.java
@@ -0,0 +1,11 @@
+/*
+Which of the following is the correct syntax for a method header with parameters?
+a. public static void example(x, y) {
+b. public static (int x, int y) example() {
+c. public static void example(int x, int y) {
+d. public static void example(x: int, y: int) {
+e. public static void example(int x,y) {
+(order shuffled)
+*/
+
+c. public static void example(int x, int y) {
