9dfc88d945a996596b66cefa38edc776d3129f6a
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-11-11 00:33:05 +0900

    Create BJP4 Self-Check 3.11: termForPrintln.java

---
+++ BJP4 Chapter 3: Parameters and Objects/BJP4 Self-Check 3.11: termForPrintln.java
@@ -0,0 +1,11 @@
+/*
+The System.out.println command works on many different types of values, including ints and doubles. What is the term for such a method?
+a. overrate
+b. overload
+c. overwrite
+d. override
+e. parameter
+(order shuffled)
+*/
+
+b. overload
