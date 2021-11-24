fc9f3ce8a54e7367fe6cf0f6f7b0ccc04c5fff2c
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-11-24 20:14:36 +0900

    Create BJP4 Self-Check 7.01: arrayDeclarationSyntax.java

---
+++ BJP4 Chapter 7: Arrays/BJP4 Self-Check 7.01: arrayDeclarationSyntax.java
@@ -0,0 +1,11 @@
+/*
+Which of the following choices is the correct syntax for declaring/initializing an array of ten integers?
+a. int[] a = new int[10];
+b. int[10] a = new int[10];
+c. int a[10] = new int[10];
+d. int a[10];
+e. []int a = [10]int;
+(order shuffled)
+*/
+
+a. int[] a = new int[10];
