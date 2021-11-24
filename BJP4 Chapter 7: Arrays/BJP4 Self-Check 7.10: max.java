2f56122c0f90b798bb16d521d23dc69a067c4624
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-11-24 20:26:40 +0900

    Create BJP4 Self-Check 7.10: max.java

---
+++ BJP4 Chapter 7: Arrays/BJP4 Self-Check 7.10: max.java
@@ -0,0 +1,15 @@
+/*
+Write a method called max that accepts an array of integers as a parameter and returns the maximum value in the array. 
+For example, if the array passed stores {12, 7, -1, 25, 3, 9}, your method should return 25. 
+You may assume that the array contains at least one element. Your method should not modify the elements of the array.
+*/
+
+public static int max (int [] data) {
+    int maximum = data[0];
+    for(int i = 0; i < data.length; i++) {
+        if(data[i] > maximum) {
+            maximum = data[i];
+        }
+    }
+    return maximum;
+}
