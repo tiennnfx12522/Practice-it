3b8fff5398a20628670b3a48cdc0e20cfbce6d56
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-11-24 20:15:56 +0900

    Create BJP4 Self-Check 7.02: numbersElements.java

---
+++ BJP4 Chapter 7: Arrays/BJP4 Self-Check 7.02: numbersElements.java
@@ -0,0 +1,17 @@
+/*
+Assume that the following array has been defined, though you do not necessarily know its length.
+
+int[] numbers = new int[??];
+What expression should be used:
+
+To access the first element?	
+
+To access the last element, assuming was created with length 10?	
+
+To access its last element, regardless of its length?	
+
+*/
+
+numbers[0]
+numbers[9]
+numbers[numbers.length-1]
