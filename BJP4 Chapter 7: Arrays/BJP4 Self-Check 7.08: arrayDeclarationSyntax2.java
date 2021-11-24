07f9db12e9043c6a0d1fa01888370abe02fcbfde
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-11-24 20:22:24 +0900

    Create BJP4 Self-Check 7.08: arrayDeclarationSyntax2.java

---
+++ BJP4 Chapter 7: Arrays/BJP4 Self-Check 7.08: arrayDeclarationSyntax2.java
@@ -0,0 +1,11 @@
+/*
+Which of the following choices is the correct syntax for quickly declaring/initializing an array of six integers to store a particular list of values?
+a. int[] a = {17, -3, 42, 5, 9, 28};
+b. int[6] a = {17, -3, 42, 5, 9, 28};
+c. int[] a = new {17, -3, 42, 5, 9, 28} [6];
+d. int a {17, -3, 42, 5, 9, 28};
+e. int[] a = new int[6] {17, -3, 42, 5, 9, 28};
+(order shuffled)
+*/
+
+a. int[] a = {17, -3, 42, 5, 9, 28};
