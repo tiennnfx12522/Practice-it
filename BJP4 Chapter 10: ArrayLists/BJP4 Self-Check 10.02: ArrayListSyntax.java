74c6e1e2805edb03e708663de84ab6fd5cd104f3
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-11-28 21:28:43 +0900

    Create BJP4 Self-Check 10.02: ArrayListSyntax.java

---
+++ BJP4 Chapter 10: ArrayLists/BJP4 Self-Check 10.02: ArrayListSyntax.java
@@ -0,0 +1,11 @@
+/*
+Which of the following is the correct syntax to construct an ArrayList to store integers?
+a. ArrayList<Integer> list = new ArrayList<Integer>();
+b. ArrayList list<integer> = new ArrayList<integer>();
+c. ArrayList[int] list = new ArrayList[int]();
+d. ArrayList<Integer> list = new ArrayList();
+e. ArrayList list = new ArrayList();
+(order shuffled)
+*/
+
+a. ArrayList<Integer> list = new ArrayList<Integer>();
