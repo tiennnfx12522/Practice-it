0913ef0326cd4df04aad19addf08fff7d1ff5878
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-12-03 01:26:36 +0900

    Create BJP4 Self-Check 4.03: ifStatementSyntax.java

---
+++ BJP4 Chapter 4: Conditional Execution/BJP4 Self-Check 4.03: ifStatementSyntax.java
@@ -0,0 +1,11 @@
+/*
+Which of the following if statement headers uses the correct syntax?
+a. if x = 10 then {
+b. if (x == y) {
+c. if [x == 10] {
+d. if (x => y) {
+e. if (x equals 42) {
+(order shuffled)
+*/
+
+b. if (x == y) {
