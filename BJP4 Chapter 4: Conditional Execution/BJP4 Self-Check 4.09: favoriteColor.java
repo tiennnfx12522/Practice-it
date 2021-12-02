9ac45db29a5dcfe67e02c154eb88c4cbd5c4a106
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-12-03 01:51:59 +0900

    Create BJP4 Self-Check 4.09: favoriteColor.java

---
+++ BJP4 Chapter 4: Conditional Execution/BJP4 Self-Check 4.09: favoriteColor.java
@@ -0,0 +1,19 @@
+/*
+The following code contains a bug. Examine the code and figure out the case(s) in which the code would behave incorrectly.
+Then correct the bug in the code.
+
+Type your solution here:
+Scanner console = new Scanner(System.in);
+System.out.print("What is your favorite color? ");
+String name = console.next();
+if (name == "blue") {
+    System.out.println("Mine, too!");
+}
+*/
+
+Scanner console = new Scanner(System.in);
+System.out.print("What is your favorite color? ");
+String name = console.next();
+if (name.equals("blue")) {
+    System.out.println("Mine, too!");
+}
