2709489a9026fe92499893ad192e0d2e810e0d78
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-12-04 01:15:40 +0900

    Create BJP4 Self-Check 4.18: doubleSumError.java

---
+++ BJP4 Chapter 4: Conditional Execution/BJP4 Self-Check 4.18: doubleSumError.java
@@ -0,0 +1,11 @@
+/*
+The expression (0.2 + 1.2 + 2.2 + 3.2) should equal 6.8, but in Java it does not. Why not?
+a. Java has a bug making it unable to add real numbers accurately, unlike other superior languages such as C++.
+b. Real numbers are represented as imprecise floating-point numbers in the computer, and the limited precision led to a roundoff error.
+c. Java is unable to perform a + expression involving so many numbers, so the code does not compile.
+d. Those numbers are integers in Java, so the sum is 6.
+e. The programmer forgot to cast the expression to type double, so the result is inaccurate.
+(order shuffled)
+*/
+
+b. Real numbers are represented as imprecise floating-point numbers in the computer, and the limited precision led to a roundoff error.
