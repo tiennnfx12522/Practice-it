eb25c6a060889e4b7378e84980f58ea27e26db40
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-12-04 01:27:34 +0900

    Create BJP4 Self-Check 4.20: charMystery.java

---
+++ BJP4 Chapter 4: Conditional Execution/BJP4 Self-Check 4.20: charMystery.java
@@ -0,0 +1,23 @@
+/*
+What output is produced by the following program?
+public class CharMystery {
+    public static void printRange(char startLetter, char endLetter) {
+        for (char letter = startLetter; letter <= endLetter; letter++) {
+            System.out.print(letter);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        printRange('e', 'g');
+        printRange('n', 's');
+        printRange('z', 'a');
+        printRange('q', 'r');
+    }
+}
+*/
+
+efg
+nopqrs
+
+qr
