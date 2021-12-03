872e97342e617d4bfc6a82a2269203701f0393f9
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-12-04 01:39:45 +0900

    Create BJP4 Self-Check 4.21: capitalLetter.java

---
+++ BJP4 Chapter 4: Conditional Execution/BJP4 Self-Check 4.21: capitalLetter.java
@@ -0,0 +1,11 @@
+/*
+Write an if statement that tests to see whether a String stored in a variable named phrase begins with a capital letter. 
+If the string begins with a capital letter, print capital . If not, print not capital . 
+Use at least one method call from the Character class in your solution. You may assume that the string is non-empty (contains at least one character).
+*/
+
+if (Character.isUpperCase(phrase.charAt(0))) {
+    System.out.print("capital");
+} else {
+    System.out.print("not capital");
+}
