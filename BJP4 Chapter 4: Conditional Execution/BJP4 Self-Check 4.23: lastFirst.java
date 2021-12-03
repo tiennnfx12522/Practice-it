572ff2cca59e785f1b7b7535151c1d0b83e40632
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-12-04 01:57:21 +0900

    Create BJP4 Self-Check 4.23: lastFirst.java

---
+++ BJP4 Chapter 4: Conditional Execution/BJP4 Self-Check 4.23: lastFirst.java
@@ -0,0 +1,11 @@
+/*
+Write a method named lastFirst that accepts a string as its parameter representing a person's first and last name. 
+The method should return the person's last name followed by the first initial and a period. 
+For example, the call lastFirst("Marla Singer") should return "Singer, M." . 
+You may assume that the string passed consists of exactly two words separated by a single space.
+*/
+
+public static String lastFirst (String inputName) {
+    String[] parts = inputName.split(" ");
+    return parts[1] + ", " + parts[0].charAt(0) + ".";
+}
