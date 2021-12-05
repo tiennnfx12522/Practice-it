0bdb655b779937cc35820a70e1f2369e2399827f
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-12-05 22:55:22 +0900

    Create BJP4 Self-Check 4.26: getGradePreconditions.java

---
+++ BJP4 Chapter 4: Conditional Execution/BJP4 Self-Check 4.26: getGradePreconditions.java
@@ -0,0 +1,19 @@
+/*
+Consider a method getGrade that accepts an integer representing a student's grade percentage in a course and returns that student's numerical course grade:
+
+public static double getGrade(int percentage) {
+    ...
+}
+The grade can be between 0.0 (failing) and 4.0 (perfect).
+
+ Sound F/X
+Which of the following are precondition(s) that are appropriate for this method? Check all that apply.
+a. The parameter can be an integer or a real number.
+b. The parameter's value must be less than or equal to 100.
+c. The parameter's value must be between 0.0 and 4.0.
+d. The parameter must be a letter grade of 'A', 'B', 'C', or 'D'.
+e. The parameter's value must be greater than or equal to 0.
+(order shuffled)
+*/
+b. The parameter's value must be less than or equal to 100.
+e. The parameter's value must be greater than or equal to 0.
