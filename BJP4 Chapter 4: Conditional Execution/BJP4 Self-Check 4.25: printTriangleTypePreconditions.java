76e734375b6d261c98e2469b96166f70787c9604
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-12-05 22:51:04 +0900

    Create BJP4 Self-Check 4.25: printTriangleTypePreconditions.java

---
+++ BJP4 Chapter 4: Conditional Execution/BJP4 Self-Check 4.25: printTriangleTypePreconditions.java
@@ -0,0 +1,21 @@
+/*
+Consider a method printTriangleType that accepts three integer arguments representing the lengths of the sides of a triangle and prints the type of triangle that these sides form.
+
+public static void printTriangleType(int a, int b, int c) {
+    ...
+}
+The three types are equilateral, isosceles, and scalene. An equilateral triangle has three sides of the same length, an isosceles triangle has two sides that are the same length, and a scalene triangle has three sides of different lengths. However, certain integer values (or combinations of values) would be illegal and could not represent the sides of an actual triangle. What are these values? (In other words, how would you describe the precondition(s) of the printTriangleType method?)
+
+ Sound F/X
+Which of the following are precondition(s) that are appropriate for this method? Check all that apply.
+a. All sides must have the same length.
+b. No two sides' lengths can be the same.
+c. The three side lengths must be positive integers.
+d. The sum of the sides' lengths must be an even number.
+e. No side's length may exceed the sum of any two other side lengths.
+f. No side's length can be greater than 180.
+(order shuffled)
+*/
+
+c. The three side lengths must be positive integers.
+e. No side's length may exceed the sum of any two other side lengths.
