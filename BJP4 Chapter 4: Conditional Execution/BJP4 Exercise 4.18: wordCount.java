2eae0587d9bad9a174e176411122aaf527402d7a
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-12-07 23:35:25 +0900

    Create BJP4 Exercise 4.18: wordCount.java

---
+++ BJP4 Chapter 4: Conditional Execution/BJP4 Exercise 4.18: wordCount.java
@@ -0,0 +1,17 @@
+/*
+Write a method called wordCount that accepts a String as its parameter and returns the number of words in the String. 
+A word is a sequence of one or more nonspace characters (any character other than ' '). For example, the call wordCount("hello") should return 1, 
+the call wordCount("how are you?") should return 3, 
+the call wordCount(" this     string has     wide       spaces ") should return 5, and the call wordCount(" ") should return 0.
+*/
+
+public static int wordCount(String input) {
+	String[] str = input.split(" ");
+	int Count = 0;
+	for(String s : str) {
+		if(s.length() > 0) {
+		Count++;
+	}
+	}
+	return Count;
+}
