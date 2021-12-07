aa8fbb137b347c351ecbb0d1c5aecbf5dbe8f3bc
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-12-07 22:16:32 +0900

    Create BJP4 Exercise 4.2: repl.java

---
+++ BJP4 Chapter 4: Conditional Execution/BJP4 Exercise 4.2: repl.java
@@ -0,0 +1,19 @@
+/*
+Write a method named repl that accepts a String and a number of repetitions as parameters and returns the String concatenated that many times. 
+For example, the call repl("hello", 3) returns "hellohellohello".
+If the number of repetitions is 0 or less, an empty string is returned.
+*/
+
+public static String repl (String input, int rep) {
+    if (rep <= 0) {
+        return "";
+    } else {
+        String result = "";
+        for (int i = 1; i <= rep; i++) {
+            result += input;
+        } 
+        return result; 
+    }
+}
+
+
