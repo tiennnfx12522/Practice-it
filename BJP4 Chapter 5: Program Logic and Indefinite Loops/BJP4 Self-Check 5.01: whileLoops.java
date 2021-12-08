f37e98fd824434245c80b724a26926894e37d2ed
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-12-09 00:56:57 +0900

    Create BJP4 Self-Check 5.01: whileLoops.java

---
+++ BJP4 Chapter 5: Program Logic and Indefinite Loops/BJP4 Self-Check 5.01: whileLoops.java
@@ -0,0 +1,35 @@
+/*
+For each of the following while loops, how many times will the loop execute its body?
+Remember that "zero," "infinity," and "unknown" are legal answers.
+
+a)
+int x = 1;
+while (x < 100) {
+    System.out.print(x + " ");
+    x += 10;
+}
+b)
+int max = 10;
+while (max < 10) {
+    System.out.println("count down: " + max);
+    max--;
+}
+c)
+int x = 250;
+while (x % 3 != 0) {
+    System.out.println(x);
+}
+d)
+int x = 2;
+while (x < 200) {
+    System.out.print(x + " ");
+     x *= x;
+}
+*/
+
+a. 10
+b. zero
+c. infinity
+d. 3
+e. 5
+f. 7
