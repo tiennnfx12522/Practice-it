091c922db982dd4342ccd54c3539b2f590629d73
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-12-09 01:47:54 +0900

    Create BJP4 Self-Check 5.09: doWhileSeashore.java

---
+++ BJP4 Chapter 5: Program Logic and Indefinite Loops/BJP4 Self-Check 5.09: doWhileSeashore.java
@@ -0,0 +1,21 @@
+/*
+Write a do/while loop that repeatedly prints a certain message until the user tells the program to stop. 
+The do/while is appropriate because the message should always be printed at least one time, 
+even if the user types n after the first message appears. The message to be printed is as follows:
+
+She sells seashells by the seashore.
+Do you want to hear it again? y
+She sells seashells by the seashore.
+Do you want to hear it again? y
+She sells seashells by the seashore.
+Do you want to hear it again? n
+
+*/
+
+Scanner sc = new Scanner(System.in);
+String input;
+do {
+    System.out.println("She sells seashells by the seashore.");
+    System.out.print("Do you want to hear it again? ");
+   input = sc.nextLine();
+} while (input.equals("y"));
