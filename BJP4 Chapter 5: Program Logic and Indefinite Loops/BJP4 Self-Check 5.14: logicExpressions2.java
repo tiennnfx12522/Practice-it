811c344cae2f0f7bcccb925a8a52b1b9128553bd
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-12-09 02:35:41 +0900

    Create BJP4 Self-Check 5.14: logicExpressions2.java

---
+++ BJP4 Chapter 5: Program Logic and Indefinite Loops/BJP4 Self-Check 5.14: logicExpressions2.java
@@ -0,0 +1,45 @@
+/*
+Given the following variable declarations:
+
+int x = 27;
+int y = -1;
+int z = 32;
+boolean b = false;
+What is the value of each of the following boolean expressions?
+
+!b	
+b || true	
+(x > y) && (y > z)	
+(x == y) || (x <= z)	
+!(x % 2 == 0)	
+(x % 2 != 0) && b	
+b && !b	
+b || !b	
+(x < y) == b	
+!(x / 2 == 13) || b || (z * 3 == 96)	
+(z < x) == false	
+!((x > 0) && (y < 0))	
+
+*/
+
+Given the following variable declarations:
+
+int x = 27;
+int y = -1;
+int z = 32;
+boolean b = false;
+What is the value of each of the following boolean expressions?
+
+ Sound F/X
+!b ---> true
+b || true ---> true
+(x > y) && (y > z) ---> false
+(x == y) || (x <= z) ---> true
+!(x % 2 == 0) ---> true
+(x % 2 != 0) && b	---> false
+b && !b	---> false
+b || !b	---> true
+(x < y) == b ---> true
+!(x / 2 == 13) || b || (z * 3 == 96) ---> true
+(z < x) == false ---> true
+!((x > 0) && (y < 0)) ---> false
