c20578992d662b822d636b73f46c6eb0edbb8e0c
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-12-09 21:46:17 +0900

    Create BJP4 Self-Check 7.12: traversal.java

---
+++ BJP4 Chapter 7: Arrays/BJP4 Self-Check 7.12: traversal.java
@@ -0,0 +1,21 @@
+/*
+Which of the following best describes an array traversal?
+a. Constructing a new empty array.
+b. Deleting values from an array.
+c. Changing the elements into the opposite order.
+d. A sequential processing of each of an array's elements.
+e. Changing the elements from positive to negative.
+(order shuffled)
+
+Which of the following are examples of problems that can be solved by traversing an array?
+a. Printing an array's elements.
+b. Comparing two arrays for equality.
+c. Swapping the first and last values of an array.
+d. Examining the value of the first element of the array.
+e. Searching an array for a given value.
+(order shuffled)
+*/
+
+d
+
+a,b,e
