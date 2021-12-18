bf9a77e019e751bbed201fc6cf6ed5114768d765
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-12-18 13:27:22 +0900

    Create BJP4 Exercise 8.05: slopePoint.java

---
+++ BJP4 Chapter 8: Classes/BJP4 Exercise 8.05: slopePoint.java
@@ -0,0 +1,25 @@
+/*
+Add the following method to the Point class:
+
+public double slope(Point other)
+
+Returns the slope of the line drawn between this Point and the given other Point. Use the formula (y2 - y1) / (x2 - x1) to determine the slope between two points (x1, y1) and (x2, y2). Note that this formula fails for points with identical x-coordinates, so throw an IllegalArgumentException in this case.
+
+public class Point {
+    private int x;
+    private int y;
+    
+    // // your code goes here
+    
+}
+*/
+
+public double slope(Point p) {
+    if (this.x == p.x) {
+        throw new IllegalArgumentException();
+    } else {
+        double dy = (double)(this.y - p.y);
+        double dx = (double)(this.x - p.x);
+        return dy / dx;
+    }
+}
