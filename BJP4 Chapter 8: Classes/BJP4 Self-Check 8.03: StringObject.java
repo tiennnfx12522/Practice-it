6c89d6bc6d8ac3422c616ed40b8591ff1b952713
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-12-22 02:04:53 +0900

    Create BJP4 Self-Check 8.03: StringObject.java

---
+++ BJP4 Chapter 8: Classes/BJP4 Self-Check 8.03: StringObject.java
@@ -0,0 +1,14 @@
+/*
+What is the state and behavior of a String object?
+a. The state is its sequence of characters, and the behavior is its methods, such as length and indexOf.
+b. The state is the string itself, and the behavior is what you do with it, such as putting it in an array or list.
+c. The state is the location the text came from, such as a file or from the Scanner, and the behavior is when you 
+save that string to a new location, such as to a new file.
+d. The state is the memory occupied by the object, and the behavior is what you do with that memory, 
+such as storing characters in it.
+e. The state is methods that return values, such as length or charAt, and the behavior is methods that change the string,
+such as toUpperCase or substring.
+(order shuffled)
+*/
+
+a. The state is its sequence of characters, and the behavior is its methods, such as length and indexOf.
