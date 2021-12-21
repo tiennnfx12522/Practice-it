2721250cf64e8f4fd271e2390c4f57b6075b5a6f
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-12-22 02:02:56 +0900

    Create BJP4 Self-Check 8.01: whatIsOOP.java

---
+++ BJP4 Chapter 8: Classes/BJP4 Self-Check 8.01: whatIsOOP.java
@@ -0,0 +1,15 @@
+/*
+Which of the following is an important difference between object-oriented programming and procedural programming?
+a. Object-oriented programming was invented by Microsoft Corporation while procedural programming is 
+invented by Apple for use on Macs.
+b. Procedural programming treats a program as a sequence of actions or commands, while object-oriented programming 
+looks at a program as a group of interacting entities named objects with related data and behavior.
+c. Object-oriented programming is only possible in Java and not in other languages such as C++.
+d. Object-oriented programming is more powerful and capable of solving a larger class of computational problems 
+than procedural programming, which is much more limited and unable to be used in large projects.
+e. Procedural programming is slower and less efficient than object-oriented programming.
+(order shuffled)
+*/
+
+b. Procedural programming treats a program as a sequence of actions or commands, while object-oriented programming 
+looks at a program as a group of interacting entities named objects with related data and behavior.
