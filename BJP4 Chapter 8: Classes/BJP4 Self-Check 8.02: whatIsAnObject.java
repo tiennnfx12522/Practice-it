67787bb5b1a67503bb7a9ad3eeec2665f0eae54f
Nguyen Ngoc Tien <93525819+tiennnfx12522@users.noreply.github.com>
2021-12-22 02:04:00 +0900

    Create BJP4 Self-Check 8.02: whatIsAnObject.java

---
+++ BJP4 Chapter 8: Classes/BJP4 Self-Check 8.02: whatIsAnObject.java
@@ -0,0 +1,13 @@
+/*
+What is an object? How is an object different from a class?
+a. An object is a kind of class that does not contain any behavior (methods).
+b. An object is not encapsulated and a class is encapsulated, making classes more powerful and reusable than objects.
+c. An object is an entity that encapsulates related data and behavior, while a class is the blueprint for a type of 
+objects.
+d. A class is an instance of an object. One object can be used to create many classes.
+e. Objects are used in object-oriented programming and classes are used in class-oriented programming.
+(order shuffled)
+*/
+
+c. An object is an entity that encapsulates related data and behavior, while a class is the blueprint for a type of 
+objects.
